229845444d052127ca26953a0f4b17cb768e065d
James Arjay Milanes <31022654+ugipako@users.noreply.github.com>
2017-12-08 11:08:38 +0800

    Add files via upload

---
+++ www/js/process.js
@@ -0,0 +1,207 @@
+let process = {
+	menu:function(){
+		let content = <ul>
+			<center><h1><a href='#' onClick={process.page1}><button>Click Me!!!!</button></a></h1></center>
+		</ul>;
+		ReactDOM.render(content,document.getElementById('root'));
+	},
+	page1:function(){
+		let content = <div><center>
+				<font color="white"><h1>Terrence Bill Vitanzos Romeo</h1></font><br/> <img src="img/1.jpg" height="400" width="400"></img><br/>
+				
+				
+				<a href='#' onClick={process.page10}><button>prev</button></a>======
+				<a href='#' onClick={process.page2}><button>next</button></a>
+				
+<font color="white"><h3>
+Terrence Bill Vitanzos Romeo (born March 16, 1992) is a Filipino professional<br/>
+ basketball player for the GlobalPort Batang Pier in the Philippine Basketball Association (PBA).<br/>
+  He plays both the point guard and shooting guard positions.[1]<br/>
+<br/>
+He played as a point guard for the Far Eastern University before being selected fifth overall in <br/>
+the 2013 PBA draft by GlobalPort.[2] He won the 2011 UAAP Rookie of the Year, was a member of the <br/>
+UAAP Mythical Team during his junior and senior years, and was the 2014 UAAP mens basketball Most Valuable Player.[3]<br/>
+			</h3></font></center></div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	},
+	page2:function(){
+		let content = <div>
+				<center>
+				<font color="white"><h1>Matthew Wright</h1></font><br/> <img src="img/2.jpg" height="400" width="400"></img><br/>
+				
+				<a href='#' onClick={process.page1}><button>prev</button></a>======
+				<a href='#' onClick={process.page3}><button>next</button></a><br/>
+				<font color="white"><h3>
+				Matthew Andrew Cristopher Wright (born February 7, 1991) is a <br/>
+				Filipino-Canadian professional basketball player for the Phoenix Fuel Masters<br/>
+				 of the Philippine Basketball Association (PBA). He played collegiate basketball<br/>
+				  at St. Bonaventure University.[2]He has a girlfriend, college sweetheart, Gabriela <br/>
+				  Moscoso and a son (Preston Wright).<br/>
+				</h3></font>
+		</center>
+			</div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	},
+	page3:function(){
+		let content = <div>
+				<center>
+				<font color="white"><h1>Gabriel Daniel V. "Gabe" Norwood</h1></font><br/> <img src="img/3.jpg" height="400" width="400"></img><br/>
+				
+				<a href='#' onClick={process.page2}><button>prev</button></a>======
+				<a href='#' onClick={process.page4}><button>next</button></a><br/>
+					<font color="white"><h3>
+					Gabriel Daniel V. "Gabe" Norwood (born February 9, 1985) is a Filipino<br/>
+					 American professional basketball player and a former NCAA basketball player<br/>
+					  at George Mason University. Norwood is currently playing for the Rain or Shine<br/>
+					   Elasto Painters in the Philippine Basketball Association. He is also a member of<br/> 
+					   the Philippine national basketball team on multiple occasions.<br/>
+					</h3></font>
+			</center>
+			</div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	},
+	page4:function(){
+	let content = <div>
+				<center>
+				<font color="white"><h1>Raymond Almazan</h1></font><br/> <img src="img/4.jpg" height="400" width="400"></img><br/>
+				
+				<a href='#' onClick={process.page3}><button>prev</button></a>======
+				<a href='#' onClick={process.page5}><button>next</button></a><br/>
+					<font color="white"><h3>
+					Raymond C. Almazan (born August 2, 1989) is a Filipino professional<br/>
+					 basketball player for the Rain or Shine Elasto Painters of the Philippine Basketball Association (PBA).<br/>
+					</h3></font>
+			</center>
+			</div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	},
+	page5:function(){
+	let content = <div>
+				<center>
+				<font color="white"><h1>June Mar Fajardo</h1></font><br/> <img src="img/5.jpg" height="400" width="400"></img><br/>
+				
+				<a href='#' onClick={process.page4}><button>prev</button></a>======
+				<a href='#' onClick={process.page6}><button>next</button></a><br/>
+				<font color="white"><h3>
+June Mar Fajardo (born November 17, 1989) is a Filipino professional basketball player<br/>
+ for the San Miguel Beermen of the Philippine Basketball Association (PBA).<br/>
+<br/>
+Born in Compostela, he transferred with his parents to Pinamungajan at an early age,<br/> 
+where he spent most of his younger and teen years. He played center for the University of<br/>
+ Cebu Webmasters in the CESAFI and for the San Miguel Beermen in the ASEAN Basketball League <br/>
+ before being selected as the first overall in the 2012 PBA draft by Petron Blaze Boosters.<br/>
+  Despite his young age, Fajardo showed great potential and has been dubbed by local sports <br/>
+  analysts as the Future of Philippine basketball. During his rookie season, he earned a silver<br/>
+   medal playing Mens basketball at the 2013 FIBA Asia Championship for Team Philippines.<br/>
+    He was also selected to the All-Rookie Team and Second Mythical Team. He is known as The Kraken.<br/>
+				</h3></font>
+				
+				
+			</center>
+			</div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	},
+	page6:function(){
+	let content = <div>
+				<center>
+				<font color="white"><h1>Jayson Castro</h1></font><br/> <img src="img/6.jpg" height="400" width="400"></img><br/>
+				
+				<a href='#' onClick={process.page5}><button>prev</button></a>======
+				<a href='#' onClick={process.page7}><button>next</button></a><br/>
+				
+				<font color="white"><h3>
+Jayson Castro William[1][2] (born June 30, 1986) is a Filipino professional basketball player<br/>
+ for the TNT KaTropa of the Philippine Basketball Association (PBA), known as The Blur for his speed.[3]<br/>
+<br/>
+He played for the Philippine Christian University Dolphins in the National Collegiate Athletic Association,<br/>
+ Philippine Basketball League teams Hapee-PCU Teeth Protectors and Harbour Centre Batang Pier.<br/>
+				</h3></font>
+			</center>
+			</div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	},
+	page7:function(){
+	let content = <div>
+				<center>
+				<font color="white"><h1>Roger Ray Pogoy</h1></font><br/> <img src="img/7.jpg" height="400" width="400"></img><br/>
+				
+				<a href='#' onClick={process.page6}><button>prev</button></a>======
+				<a href='#' onClick={process.page8}><button>next</button></a><br/>
+
+				<font color="white"><h3>
+Roger Ray Pogoy (born June 16, 1992) is a Filipino professional basketball player <br/>
+for the TNT KaTropa of the Philippine Basketball Association (PBA).<br/>
+				</h3></font>
+			</center>
+			</div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	},
+	page8:function(){
+	let content = <div>
+				<center>
+				<font color="white"><h1>Troy Rosario</h1></font><br/> <img src="img/8.jpg" height="400" width="400"></img><br/>
+				
+				<a href='#' onClick={process.page7}><button>prev</button></a>======
+				<a href='#' onClick={process.page9}><button>next</button></a><br/>
+				
+				<font color="white"><h3>
+Jeth Troy U. Rosario (born January 20, 1992) is a Filipino professional basketball player for <br/>
+the TNT KaTropa of the Philippine Basketball Association (PBA).<br/>
+				</h3></font>
+			</center>
+			</div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	},
+	page9:function(){
+	let content = <div>
+				<center>
+				<font color="white"><h1>Japeth Aguilar</h1></font><br/> <img src="img/9.JPG" height="400" width="400"></img><br/>
+				
+				<a href='#' onClick={process.page8}><button>prev</button></a>======
+				<a href='#' onClick={process.page10}><button>next</button></a><br/>
+				<font color="white"><h3>
+Japeth Paul Cabrera Aguilar (born January 25, 1987) is a Filipino professional basketball <br/>
+player for the Barangay Ginebra San Miguel of the Philippine Basketball Association (PBA).<br/>
+ He first played collegiate basketball for the Ateneo Blue Eagles in the University Athletic<br/>
+  Association of the Philippines (UAAP), but after two seasons, he moved to the Western Kentucky<br/>
+   University Hilltoppers in the Division I of the National Collegiate Athletic Association in the United States.<br/>
+<br/>
+Aguilars father, Peter Aguilar, was a former professional basketball player in the PBA.<br/>
+				</h3></font>
+			</center>
+			</div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	},
+	page10:function(){
+	let content = <div>
+				<center>
+				<font color="white"><h1>Marc Pingris</h1></font><br/> <img src="img/10.jpg" height="400" width="400"></img><br/>
+				
+				<a href='#' onClick={process.page9}><button>prev</button></a>======
+				<a href='#' onClick={process.page1}><button>next</button></a><br/>
+				
+				<font color="white"><h3>
+Jean Marc Pingris, Jr.[1] (born October 16, 1981) is a Filipino professional basketball player <br/>
+for the Magnolia Hotshots of the Philippine Basketball Association (PBA).<br/>
+<br/>
+Born in Pozorrubio, Pangasinan, Pingris started his career in the PBA at the 2004 PBA draft which featured<br/>
+ James Yap, Gary David, Ranidel de Ocampo, Rich Alvarez and Nelbert Omolon. Pingris was drafted by the FedEx<br/>
+  Express with the 3rd overall pick, but was quickly traded to the Purefoods TJ Hotdogs along with Egay Billones.<br/>
+   It is with Purefoods that Pingris established his reputation as perhaps the best defender in the PBA today.<br/>
+				</h3></font>
+			</center>
+			</div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	},
+	loading:function(){
+		let content = <div><center><h1><font color="white">
+					ball is life....
+				</font></h1></center></div>;
+		ReactDOM.render(content,document.getElementById('root'));
+	}
+}
+
+process.loading();
+setTimeout(function(){
+	process.menu();
+},500);
